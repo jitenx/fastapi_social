8274db9182e11ae3209a999a3d685c09d5365e8f
jitenx <jituaec@yahoo.com>
2025-12-02 15:31:46 +0530

    updated alembic

--- app/config/config.py
+++ app/config/config.py
@@ -2,6 +2,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class AppSettings(BaseSettings):
+    # database_version: str
     database_host: str
     database_port: str
     database_name: str
