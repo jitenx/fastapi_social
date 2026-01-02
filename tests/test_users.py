c60bee2030bc6eaf856fcdaa66d39ca154340e11
jitenx <jituaec@yahoo.com>
2026-01-03 00:26:09 +0530

    added test files and folder to git

---
+++ tests/test_users.py
@@ -0,0 +1,61 @@
+import pytest
+from jose import jwt
+from app.schemas import schemas
+
+from app.config.config import settings
+
+
+# def test_root(client):
+
+#     res = client.get("/")
+#     print(res.json().get('message'))
+#     assert res.json().get('message') == 'Hello World'
+#     assert res.status_code == 200
+
+
+def test_create_user(client):
+    res = client.post(
+        "/users/",
+        json={
+            "email": "hello123@gmail.com",
+            "password": "password123",
+            "phone_number": "9873563210",
+            "address": "Dighwa",
+        },
+    )
+
+    new_user = schemas.User(**res.json())
+    assert new_user.email == "hello123@gmail.com"
+    assert res.status_code == 201
+
+
+def test_login_user(test_user, client):
+    res = client.post(
+        "/login",
+        data={"username": test_user["email"], "password": test_user["password"]},
+    )
+    login_res = schemas.Token(**res.json())
+    payload = jwt.decode(
+        login_res.access_token, settings.secret_key, algorithms=[settings.algorithm]
+    )
+    email = payload.get("user_email")
+    assert email == test_user["email"]
+    assert login_res.token_type == "bearer"
+    assert res.status_code == 201
+
+
+@pytest.mark.parametrize(
+    "email, password, status_code",
+    [
+        ("wrongemail@gmail.com", "password123", 403),
+        ("sanjeev@gmail.com", "wrongpassword", 403),
+        ("wrongemail@gmail.com", "wrongpassword", 403),
+        (None, "password123", 422),
+        ("sanjeev@gmail.com", None, 422),
+    ],
+)
+def test_incorrect_login(test_user, client, email, password, status_code):
+    res = client.post("/login", data={"username": email, "password": password})
+
+    assert res.status_code == status_code
+    # assert res.json().get('detail') == 'Invalid Credentials'
