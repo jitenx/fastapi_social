74c25e2d1ee00915e99e7168ab80e2d2e2071fa0
jitenx <jituaec@yahoo.com>
2026-01-06 15:34:24 +0530

    test again

--- app/main.py
+++ app/main.py
@@ -2,10 +2,10 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.routers import auth, post, user, vote
-from app.models import models
-from app.database.database import engine
+# from app.models import models
+# from app.database.database import engine
 
-models.Base.metadata.create_all(bind=engine)
+# models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 origins = ["*"]
