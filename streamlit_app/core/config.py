4c7944983493f508479287756e38e2b00bec0cdb
jitenx <jituaec@yahoo.com>
2026-02-19 19:36:01 +0530

    updated config file to get url from .env file or env variable

--- streamlit_app/core/config.py
+++ streamlit_app/core/config.py
@@ -1,5 +1,16 @@
-# API_BASE_URL = "http://127.0.0.1:8000"
-API_BASE_URL = "https://start-social-04ln.onrender.com"
+import os
+
+try:
+    from dotenv import load_dotenv
+except ImportError:
+    load_dotenv = None
+
+if load_dotenv:
+    load_dotenv()
+
+API_BASE_URL = os.getenv("API_BASE_URL", "http://127.0.0.1:8000")
+
+
 LOGIN_ENDPOINT = "/login"
 USERS_ENDPOINT = "/users"
 POSTS_ENDPOINT = "/posts"
